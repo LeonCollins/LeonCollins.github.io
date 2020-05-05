6db1ca9c9613f2accd7b74b064d8a36620a6d52b
LeonCollins <38861414+LeonCollins@users.noreply.github.com>
2020-05-05 22:40:52 +0100

    functions

--- main.js
+++ main.js
@@ -3,6 +3,16 @@ function setHeight() {
 	el.object3D.position.y = document.getElementById("height").value;
 }
 
+function setColour() {
+	var el = document.getElementById("POne");
+	el.setAttribute('material', {color: document.getElementById("colour").value});
+}
+
+function setShape() {
+	var el = document.getElementById("POne");
+	el.setAttribute('geometry', {primitive: document.getElementById("shape").value});
+}
+
 function openOverlay() {
   document.getElementById("overlay").style.width = "100%";
 }
